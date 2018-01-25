9fadb3f5338d0d328514437d13f115c4264e7a21
thisforeda <thisforeda@live.cn>
2018-01-25 18:36:09 +0800

    update test.

--- test.c
+++ test.c
@@ -2,9 +2,6 @@
 #include <time.h>
 #include "skiplist.h"
 
-#define LEVEL 32
-
-
 void skiplist_dump_all(skiplist_t* sl) {
   int32_t idx;
   skipnode_t* t;
@@ -25,15 +22,20 @@ int main(int argc, char* argv[]) {
   clock_t c;
   skiplist_t s;
   skipnode_t* t;
-  uint32_t idx, e, i, k, times;
+  uint32_t idx, e, i, k, times, level;
 
-  if (argc == 2) {
+  if (argc == 3) {
     times = atoi(argv[1]);
+    level = atoi(argv[2]);
+    if (level > UINT8_MAX)
+      level = 255;
   } else {
     times = 2000000;
+    level = 32;
   }
 
-  skiplist_init(&s, LEVEL);
+  skiplist_init(&s, level);
+  printf("while(%d) random hash test, skiplist level %d.\n", times, s.level);
 
   skiplist_insert(&s, 32, NULL);
   skiplist_insert(&s, 33, NULL);
@@ -41,27 +43,27 @@ int main(int argc, char* argv[]) {
   skiplist_insert(&s, 35, NULL);
   skiplist_insert(&s, 36, NULL);
 
-  if ((t = skiplist_find(&s, 35)) != NULL) {
-    printf("skiplist_find(&s, 35) = %d, %p\n", t->key, t);
-  }
-  if ((t = skiplist_find(&s, 36)) != NULL) {
-    printf("skiplist_find(&s, 35) = %d, %p\n", t->key, t);
-  }
-
+  printf("\n[DUMP NODES]\n");
   skiplist_dump_all(&s);
 
   skiplist_delete(&s, 33);
   skiplist_delete(&s, 36);
-  printf("after free key 33, 36\n");
+  printf("\n[AFTER FREE HASH 33, 36]\n");
+  if ((t = skiplist_find(&s, 35)) != NULL) {
+    printf("skiplist_find(&s, 35) = found, @%p\n", t);
+  }
   if ((t = skiplist_find(&s, 36)) == NULL) {
     printf("skiplist_find(&s, 36) = not found\n");
+  } else {
+    printf("deleted key 36 found!\n");
   }
+  printf("\n[DUMP NODES]\n");
   skiplist_dump_all(&s);
   skiplist_destroy(&s);
+  printf("base test finished.\n\n");
 
-  skiplist_init(&s, LEVEL);
+  skiplist_init(&s, level);
   idx = times, i = 0, e = 0, c = clock();
-  printf("random %d hash keys\n", times);
   while (idx--) {
   	k = rand() * rand();
   	if (!skiplist_find(&s, k)){
@@ -71,21 +73,19 @@ int main(int argc, char* argv[]) {
       e ++;
     }
   }
-  printf("%d hash keys inserted, %d keys exist, %ldms\n", i, e, clock() - c);
+  printf("%d random hash keys inserted, %d keys exist, %ldms\n", i, e, clock() - c);
 
   idx = times, i = 0, e = 0, c = clock();
-  printf("finding %d random hash keys\n", times);
   while (idx--) {
     k = rand() * rand();
   	if (skiplist_find(&s, k)) {
       e ++;
     }
   }
-  printf("%d random hash keys finded, %ldms\n", e, clock() - c);
+  printf("find %d random hash keys, %ldms\n", e, clock() - c);
 
   c = clock();
-  printf("destroy skiplist, ");
   skiplist_destroy(&s);
-  printf("%ldms\n", clock() - c);
+  printf("destroy skiplist, %ldms\n", clock() - c);
   return 0;
 }
